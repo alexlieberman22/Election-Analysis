f13faf0e4b13f2b46ad8d04d5fed90e0f1e8c7cf
Alex Lieberman <alexlieberman55@hotmail.com>
2022-01-06 21:09:36 -0700

    Updated PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -1,26 +1,25 @@
-from _typeshed import ReadableBuffer
 import csv
 import os
 
 file_read = os.path.join('Resources','election_results.csv')
 file_write = os.path.join('Analysis', 'election_analysis.txt')
 
+candidate_options = []
+total_votes = 0
 
 with open(file_read) as election_data:
     reader = csv.reader(election_data)
 
     headers = next(reader)
-    then_this = next(headers)
-    print(headers)
-    print(then_this)
 
+    for row in reader:
+        total_votes += 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
 
-
-    # for row in reader:
-    #     print(row)
-
-
-
+print(total_votes)
+print(candidate_options)
 
 
 # with open(file_write, "w") as txt_file:
