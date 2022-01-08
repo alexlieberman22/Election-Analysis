931e367b752824f5b5d06f40da8caad1e2d75089
Alex Lieberman <alexlieberman55@hotmail.com>
2022-01-07 21:10:43 -0700

    Winner result found

--- PyPoll.py
+++ PyPoll.py
@@ -8,6 +8,11 @@ candidate_options = []
 candidate_votes = {}
 total_votes = 0
 
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+
 with open(file_read) as election_data:
     reader = csv.reader(election_data)
 
@@ -29,8 +34,19 @@ with open(file_read) as election_data:
    
         vote_percentage = float(votes) / float(total_votes) * 100
 
-        print(f"{candidate_name}: {vote_percentage:.1f}% ")
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+
+
+print(f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
 
 
 # with open(file_write, "w") as txt_file:
